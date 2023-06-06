9532c96c816aa2413dd9d20566e16db9c8681286
Remas Orca <remas.orca@gmail.com>
2023-06-06 20:10:03 +0300

    Add project files.

---
+++ 2304_Ulyanytskyi_01010536/2304_Ulyanytskyi_01010536.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+double get_user_value() {
+    double value;
+    cin >> value;
+    return value;
+}
+
+int main()
+{
+    const double first_distance = 1.5;
+    double result = 0;
+    double days_number;
+
+    cout << "Enter the number of days the turtle will travel: ";
+    days_number = get_user_value();
+
+    for (int i = 0; i < days_number; i++) {
+        result += first_distance + i * 0.2;
+    }
+
+    cout << "In " << days_number << " the turtle walked " << result << " meters";
+
+    return 0;
+}
